4ef7d624bbac274c781ee022aeb5e431a1ffeb67
ZZcc <2923400952@qq.com>
2018-08-16 23:31:05 +0800

    填坑

--- Arduino/README.md
+++ Arduino/README.md
@@ -2,4 +2,27 @@
 
 参加教育机器人大赛弄的 `Arduino`，把用到的坑和相关库用法记下吧
 
-开发平台为 Arduino UNO R3，拓展板为 Arduino sensor sheld V5
+## 平台说明
+
+硬件 | 名称
+-----|-------
+开发板 | Arduino UNO R3
+拓展板 | Arduino sensor sheld V5 改
+
+## 点亮你的流水灯
+
+```c
+// TODO:
+```
+
+相关教程：
+
+[数字I/O的使用](https://www.arduino.cn/thread-74476-1-1.html)
+
+[软件模拟串口通信——SoftwareSerial库的使用](https://www.arduino.cn/thread-47262-1-1.html)
+
+[print()和write()输出方式的差异](https://www.cnblogs.com/fqhy/p/7966169.html)
+
+## 进阶教程
+
+建议食用顺序：`本Readme` -> `各模块例程/IC卡`
---
+++ Arduino/各模块例程/IC卡/IC/IC.ino
@@ -0,0 +1,42 @@
+#include <SPI.h>
+#include <RFID.h>
+
+RFID rfid(10,5); // 引脚接法见readme
+void setup()
+{
+  Serial.begin(9600);
+  SPI.begin(); // 串行外设初始化
+  rfid.init(); // 读卡器初始化
+}
+
+void loop()
+{
+  // 找到卡
+  if (rfid.isCard()) {
+    Serial.println("Find the card!"); 
+    if (rfid.readCardSerial()) 
+    {
+      // 打印卡内容
+      Serial.print("The card's content is : ");
+      Serial.print(rfid.serNum[0],HEX);
+      Serial.print(rfid.serNum[1],HEX);
+      Serial.print(rfid.serNum[2],HEX);
+      Serial.print(rfid.serNum[3],HEX);
+      Serial.print(rfid.serNum[4],HEX);
+      Serial.print(rfid.serNum[5],HEX);
+      Serial.print(rfid.serNum[6],HEX);
+      Serial.print(rfid.serNum[7],HEX);
+      Serial.print(rfid.serNum[8],HEX);
+      Serial.print(rfid.serNum[9],HEX);
+
+      // 打印卡序列号
+      Serial.print("The card's number is : ");
+      Serial.print(*rfid.serNum);
+      Serial.println(" ");
+    }
+    // 选卡，可返回卡容量（锁定卡片，防止多数读取），去掉本行将连续读卡
+    rfid.selectTag(rfid.serNum);
+  }
+  // 使卡休眠 在实际使用中较少用到
+  rfid.halt();
+}
